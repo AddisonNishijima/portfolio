0b6aedddedd8eb995b6e09a0e02515f203d2a987
AddisonNishijima <addison.nishijima@gmail.com>
2017-01-13 12:23:24 -0800

    add quote, adjust css styling, add content to about page

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,5 @@ $( document ).ready(function(){
   $('.hidden').fadeIn(3000).removeClass('hidden');
   $(".button-collapse").sideNav();
   $('.carousel').carousel();
+  $("#quote").delay(2000).fadeIn();
 })
